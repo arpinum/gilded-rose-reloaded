4791674295418d1e5c89b904c665998c54a1e4b7
Jean-Baptiste Dusseaut <bodysplash@gmail.com>
2016-11-08 15:58:56 +0100

    refacto pour mettre les choses au bon endroit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,7 @@
 import auberge.infrastructure.datastore.memory.MemoireDataStore;
 import auberge.metier.Item;
 import auberge.requete.RequeteListeItems;
+import auberge.web.resource.ItemsRessource;
 import ratpack.registry.Registry;
 import ratpack.server.*;
 
@@ -20,6 +21,12 @@ public class Main {
     private static MemoireDataStore initializeGraine() {
         MemoireDataStore memoireDataStore = new MemoireDataStore();
         memoireDataStore.add(new Item("un bel item", 0, 0));
+        memoireDataStore.add(new Item("+5 Dexterity Vest", 10, 20));
+        memoireDataStore.add(new Item("Aged Brie", 2, 0));
+        memoireDataStore.add(new Item("Elixir of the Mongoose", 5, 7));
+        memoireDataStore.add(new Item("Sulfuras, Hand of Ragnaros", 0, 80));
+        memoireDataStore.add(new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20));
+        memoireDataStore.add(new Item("Conjured Mana Cake", 3, 6));
         return memoireDataStore;
     }
 }
